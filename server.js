293ea86777683b3e044fdaabef9f6135795807e1
hejiajin-boy <64115831+hejiajin-boy@users.noreply.github.com>
2020-05-01 17:47:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ var server = http.createServer(function(request, response) {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(`
-        <link rel="stylesheet" href="./style.css">
-        <h1>何加劲 </h1>
+        
+        何加劲
         `)
         response.end()
     } else if (path === '/style.css') {
